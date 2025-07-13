b78917ae7498a16a9ea24d4e8856b10a4550e507
Debanshu Ghosh <debanshughosh009@gmail.com>
2025-07-14 00:41:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from pydantic import BaseModel
 from model import predict_pipeline
 from model import __version__ as model_version
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app = FastAPI()
